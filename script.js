76534b21fc29ec553d000be4c061a92bedfcb7cf
sakshidalalsd <139738505+sakshidalalsd@users.noreply.github.com>
2023-09-21 12:25:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function getFormvalue() {
-   var first = document.getElementById('firstname').value;
-   var last = document.getElementById('lastname').value;
-   if (first == ""){
-      alert("Enter your first Name");
-   } else if(last == ""){
-      alert("Enter your last Name");
-   } else {
-      alert("First Name: " + first + "\nLast Name: " + last);
-   }
+	
+const para=document.getElementsByName("fname");
+const last=document.getElementsByName("lname");
+ alert(para[0].value+" "+ last[0].value);
 }
+  
